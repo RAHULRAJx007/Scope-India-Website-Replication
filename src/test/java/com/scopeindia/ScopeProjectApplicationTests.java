05cb96caaf9d4779d82d0d903d5aaa11cfc62f10
RAHULRAJx007 <rahulx122003@gmail.com>
2026-02-25 15:44:14 +0530

    updated location

---
+++ src/test/java/com/scopeindia/ScopeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scopeindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
