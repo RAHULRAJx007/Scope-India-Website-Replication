d14c8dcedaaabfd6ac870d280c3fca02a44a3564
RAHULRAJx007 <rahulx122003@gmail.com>
2026-03-12 01:08:50 +0530

    added student dashboard

--- src/main/java/com/scope/controller/StudentController.java
+++ src/main/java/com/scope/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.scope.controller;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +8,29 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.scope.model.Student;
+import com.scope.repository.AnnouncementRepository;
 import com.scope.repository.StudentRepository;
 
 @Controller
+@RequestMapping("/student")
 public class StudentController {
 
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @GetMapping("/dashboard")
-    public String studentDashboard(Authentication authentication, Model model) {
-
-        String email = authentication.getName();
-
-        Student student = studentRepository.findByEmail(email).orElse(null);
-
-        model.addAttribute("student", student);
-
-        return "studentdashboard";
-    }
+			@Autowired
+			StudentRepository studentRepo;
+			
+			@GetMapping("/dashboard")
+			public String dashboard(Model model, Principal principal){
+			
+			Student student = studentRepo
+			        .findByEmail(principal.getName())
+			        .orElseThrow(() -> new RuntimeException("Student not found"));
+			
+			model.addAttribute("student", student);
+			
+			return "studentdashboard";
+		}
 }
+
--- src/main/java/com/scope/repository/AnnouncementRepository.java
+++ src/main/java/com/scope/repository/AnnouncementRepository.java
@@ -1,5 +1,12 @@
 package com.scope.repository;
 
-public interface AnnouncementRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scope.model.Announcement;
+
+@Repository
+public interface AnnouncementRepository
+		extends JpaRepository<Announcement, Long> {
 
 }
